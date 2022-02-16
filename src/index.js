2dd03e27166f844474d536af6d558284a79bd61a
akarsh02 <akarsh@zeroswap.io>
2022-02-16 18:51:36 +0530

    clean up

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { MoralisProvider } from "react-moralis";
 import "./index.css";
-import QuickStart from "components/QuickStart";
 
 /** Get your free Moralis Account https://moralis.io/ */
 
@@ -13,23 +12,13 @@ const SERVER_URL = process.env.REACT_APP_MORALIS_SERVER_URL;
 const Application = () => {
   const isServerInfo = APP_ID && SERVER_URL ? true : false;
   //Validate
-  if (!APP_ID || !SERVER_URL)
-    throw new Error(
-      "Missing Moralis Application ID or Server URL. Make sure to set your .env file.",
-    );
+
   if (isServerInfo)
     return (
       <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
         <App isServerInfo />
       </MoralisProvider>
     );
-  else {
-    return (
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <QuickStart />
-      </div>
-    );
-  }
 };
 
 ReactDOM.render(
